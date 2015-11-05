7f839bcd76de5169e0cd8fbfde2b39e4c23dcc45
lachlan-00 <lachlan.00@gmail.com>
2015-11-05 14:37:47 +1000

    initial commit, mysql-connection.py added

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ampache-scripts
 just some of the things i'm playing around with in ampache
+
+Insert data from Last.fm into your ampache database
+
+Currently the project is a single script:
+ * mysql-connection.py
+
+ABOUT THE SCRIPT
+----------------
+I have used Last.fm for a a decade and i want to move away to a private solution.
+
+lastexport.py is used for exporting your listening history (also loved/banned tracks) from last.fm or libre.fm to a text file.
+http://bugs.foocorp.net/projects/librefm/wiki/LastToLibre
+https://gitorious.org/fmthings/lasttolibre
+
+(the project seems dead now so i'm going to update it to python3 when i have time to do the rest)
+https://github.com/lachlan-00/lastscrape-gui/blob/master/lastexport3.py
+
+
+USAGE
+-----
+This script that will parse a text file and add each play to the ampache database.
+If it can't match the song it will dump each row that was missed at the end so you can try again
+
+USAGE
+ * Get a dump for your username.
+     python3 ./lastexport3.py -u your_lastfm_username -o dump.txt
+ * Run the merge script and get an output dump of bad matches
+    python3 ./python3 ./mysql-connection.py > output.txt
+
---
+++ mysql-connection.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+
+""" mysql dump into ampache from tsv
+
+  merge last.fm data with ampache
+  -------------------------------
+
+  This script will examine a dump file from lastscrape
+  then query your ampache database
+
+  if it matches the artist, album and song
+  it will update your databse to reflect each play
+
+"""
+
+
+import csv
+import os
+import mysql.connector
+
+# object_count will store each play for tracks
+# with separate rows for artist & album
+selectquery = "SELECT * FROM `object_count` ORDER BY `id` DESC"
+# the song table stores artist/album as id (from artist/album tables)
+songquery = "SELECT `id` FROM `song` WHERE `title` = '"
+# get ID for song table
+albumquery = "SELECT `id` FROM `album` WHERE `name` = '"
+artistquery = "SELECT `id` FROM `artist` WHERE `name` = '"
+
+
+# TAB separated from lastscrape.py/last.fm data
+dumpfile = 'dump.txt'
+
+notfoundcount = 0
+notfoundlist = []
+
+cnx = None
+
+try:
+    cnx = mysql.connector.connect(user='dpuser', password='dbpassword',
+                                  host='127.0.0.1', database='dbname')
+except mysql.connector.errors.InterfaceError:
+    try:
+        cnx = mysql.connector.connection.MySQLConnection(user='dbuser',
+                                                         password='dbpass' +
+                                                         'word',
+                                                         host='192.168.0.1',
+                                                         database='dbname')
+    except mysql.connector.errors.InterfaceError:
+        pass
+if cnx:
+    print('Connection Established\n')
+    cursor = cnx.cursor()
+    executionlist = []
+    if os.path.isfile(dumpfile):
+        with open(dumpfile, 'r') as csvfile:
+            # lastscrape is sorted recent -> oldest so reverse that
+            # that way the database will have a lower ID for older tracks
+            openfile = reversed(list(csv.reader(csvfile, delimiter='\t',)))
+            for row in openfile:
+                tmprow = []
+                tmpartist = None
+                tmpalbum = None
+                tmpsong = None
+                foundartist = None
+                foundalbum = None
+                foundsong = None
+                try:
+                    test = row[0]
+                except IndexError:
+                    test = None
+                if test:
+                    # search ampache db for song
+                    tmpquery = (songquery + row[1].replace("'", "''") + "'")
+                    try:
+                        cursor.execute(tmpquery)
+                    except mysql.connector.errors.ProgrammingError:
+                        print('ERROR WITH QUERY:\n' + tmpquery)
+                    for rows in cursor:
+                        tmpsong = rows[0]
+                    # search ampache db for album
+                    tmpquery = (albumquery + row[3].replace("'", "''") + "'")
+                    try:
+                        cursor.execute(tmpquery)
+                    except mysql.connector.errors.ProgrammingError:
+                        print('ERROR WITH QUERY:\n' + tmpquery)
+                    for rows in cursor:
+                        tmpalbum = rows[0]
+                    # search ampache db for artist
+                    tmpquery = (artistquery + row[2].replace("'", "''") + "'")
+                    try:
+                        cursor.execute(tmpquery)
+                    except mysql.connector.errors.ProgrammingError:
+                        print('ERROR WITH QUERY:\n' + tmpquery)
+                    for rows in cursor:
+                        tmpartist = rows[0]
+
+                    # if you find all three values in the database
+                    # we have an exact match
+                    if tmpsong and tmpalbum and tmpartist:
+
+                        tmpcursor = cnx.cursor()
+
+                        # ampache creates a separate row for 
+                        # song/album/artist for each play
+                        checksong = ("SELECT * FROM `object_count` WHERE " +
+                                     "`object_type` = 'song' AND `date` = '" +
+                                     str(row[0]) + "' AND `object_id` = '" +
+                                     str(tmpsong) + "';")
+                        checkalbum = ("SELECT * FROM `object_count` WHERE" +
+                                      " `object_type` = 'album' AND `date` =" +
+                                      " '" + str(row[0]) + "' AND `object" +
+                                      "_id` = '" +
+                                      str(tmpalbum) + "';")
+                        checkartist = ("SELECT * FROM `object_count` WHERE" +
+                                       " `object_type` = 'artist' AND `date`" +
+                                       " = '" + str(row[0]) + "' AND `obje" +
+                                       "ct_id` = '" +
+                                       str(tmpartist) +
+                                       "';")
+
+                        # make sure the track is set to played in the song table
+                        setplayed = "UPDATE `song` SET `played` = 1 WHERE `id` = " + str(tmpsong) + " AND `played` = 0;"
+                        tmpcursor.execute(setplayed)
+
+                        tmpcursor.execute(checksong)
+                        for rows in tmpcursor:
+                            if tmpsong in rows:
+                                foundsong = True
+                        tmpcursor.execute(checkalbum)
+                        for rows in tmpcursor:
+                            if tmpalbum in rows:
+                                foundalbum = True
+                        tmpcursor.execute(checkartist)
+                        for rows in tmpcursor:
+                            if tmpartist in rows:
+                                foundartist = True
+                        # database already has this play recorded
+                        if foundsong and foundalbum and foundartist:
+                            pass
+                        # database is missing this play
+                        elif not foundsong and not foundalbum and not foundartist:
+                            tmpincursor = cnx.cursor()
+                            insertsong = ("INSERT INTO `ampache-debian`.`object_count` " +
+                               "(`id`, `object_type`, `object_id`, `date`, `user`, `agent`," +
+                               " `geo_latitude`, `geo_longitude`, `geo_name`, `count_type`) " +
+                               "VALUES ('0', 'song', '" + str(tmpsong) + "', '" + row[0] + "', '2'," +
+                               " NULL, NULL, NULL, NULL, 'stream');")
+                            tmpincursor.execute(insertsong)
+                            insertalbum = ("INSERT INTO `ampache-debian`.`object_count` " +
+                               "(`id`, `object_type`, `object_id`, `date`, `user`, `agent`," +
+                               " `geo_latitude`, `geo_longitude`, `geo_name`, `count_type`) " +
+                               "VALUES ('0', 'album', '" + str(tmpalbum) + "', '" + row[0] + "', '2'," +
+                               " NULL, NULL, NULL, NULL, 'stream');")
+                            tmpincursor.execute(insertalbum)
+                            insertartist = ("INSERT INTO `ampache-debian`.`object_count` " +
+                               "(`id`, `object_type`, `object_id`, `date`, `user`, `agent`," +
+                               " `geo_latitude`, `geo_longitude`, `geo_name`, `count_type`) " +
+                               "VALUES ('0', 'artist', '" + str(tmpartist) + "', '" + row[0] + "', '2'," +
+                               " NULL, NULL, NULL, NULL, 'stream');")
+                            tmpincursor.execute(insertartist)
+                         #partial match means it didn't get everything we needed
+                        else:
+                            notfoundlist.append(row)
+                    # If you don't find all 3 you don't have an exact match
+                    # so don't add these track to the database
+                    else:
+                        notfoundlist.append(row)
+                        notfoundcount = notfoundcount + 1
+    # close connections
+    csvfile.close()
+    cnx.close()
+else:
+    print('unable to connect to database')
+
+# print tracks that couldn't be integrated into the database
+for row in notfoundlist:
+    print(row)
+print(str(notfoundcount))
